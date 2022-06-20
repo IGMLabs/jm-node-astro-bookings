003dd19463845e424b9b8e790a66d2244e69a2ef
jmdiaz2 <jmdiaz2@esei.uvigo.es>
2022-06-20 12:28:13 +0200

    fs async

---
+++ src/04-fs/index.js
@@ -0,0 +1,23 @@
+import * as fs from "fs";
+
+const scriptFile = process.argv[1];
+const fileContent = fs.readFileSync(scriptFile);
+console.log(fileContent.toString());
+
+fs.readFile(scriptFile,(err, data) => {
+    if (err) {
+        console.log(`Error: ${err}`);
+    } else {
+        console.log(`Data: ${data}`);
+    }
+})
+
+function copyFile(err, fileContent){
+    if(err) {
+        console.log(`Error: ${err}`);
+    } else {
+        fs.writeFile(`${scriptFile}.copy.txt`, fileContent, () => {
+
+        });
+    }
+}
